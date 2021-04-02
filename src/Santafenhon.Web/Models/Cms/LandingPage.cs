c11bef76d124bea600954f389e703011f16b4a3f
Jojo Aquino <jfaquinojr@outlook.com>
2021-04-02 22:00:04 +0800

    Landing Layout Prototype

---
+++ src/Santafenhon.Web/Models/Cms/LandingPage.cs
@@ -0,0 +1,26 @@
+﻿using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santafenhon.Web.Models.Cms
+{
+    [PageType(Title = "Landing page")]
+    [ContentTypeRoute(Title = "Default", Route = "/landingpage")]
+    public class LandingPage: Page<LandingPage>
+    {
+        public class CategoryRegion
+        {
+            [Field]
+            public StringField Title { get; set; }
+            [Field]
+            public ImageField Image { get; set; }
+            [Field]
+            public TextField Body { get; set; }
+        }
+    }
+}
